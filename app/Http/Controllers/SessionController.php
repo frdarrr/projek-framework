15ab4ab28279be1b291f8183df76503573340900
frdarrr <firdaarinda2084@gmail.com>
2024-06-08 01:28:57 +0700

    new feature: login user

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    //
+    function index()
+    {
+        return view("sesi/index");
+
+    }
+    function login()
+    {
+
+    }
+}
